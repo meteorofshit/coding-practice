fa4c452d230aef63a65d1a1b87080708a8b9226e
Gabriel Sagula <gsagula@e-xact.com>
2015-03-31 11:15:05 -0700

    Added exercises descriptions + new array & string practice coding.

--- src/main/java/com/practice/ArrayString.java
+++ src/main/java/com/practice/ArrayString.java
@@ -3,6 +3,8 @@ package com.practice;
 public class ArrayString {
 
   public static boolean isPelindrome(String str) {
+    // Problem: Determine whether certing string is a pelindrome or 
+    // not.
     for(int i=0; i < str.length()/2; i++) {
       if(str.charAt(i) != str.charAt(str.length()-1-i)) {
         return false;
@@ -12,6 +14,7 @@ public class ArrayString {
   }
 
   public static String Reverse(String str) {
+    // Problem: Write a function that reverse a given string;
     char[] arr = str.toCharArray();
     for(int i=0; i < arr.length/2; i++) {
       int last = arr.length - 1 - i;
@@ -23,6 +26,8 @@ public class ArrayString {
   }
 
   public static char[] MergeArrays(char[] a, char[] b) {
+    // Problem: Given two sorted integer arrays A and B, merge B into 
+    // A as one sorted array.
     int i = 0, j = 0;
     char[] c = new char[a.length + b.length];
     while(i < a.length &&  j < b.length)  {
@@ -42,6 +47,8 @@ public class ArrayString {
   }
 
   public static boolean hasRepeatedLetters(String str) {
+    // Problem: Wirite a function that verified repeated letter
+    // on a given string.
     int size = 17*65536;
     boolean[] arr = new boolean[size];
     for(int i=0; i < str.length(); i++) {
@@ -53,4 +60,12 @@ public class ArrayString {
     }
     return false;
   }
+
+  public static int[] rotateToRight(int[] arr, int steps) {
+    // Problem: Rotate an array of n elements to the right by k steps. For example, 
+    // with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+    for(int i=0; i < arr.length; i++) {
+    }
+  }
+
 }
